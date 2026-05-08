6dc81f74510d7b0bbd5d0cab61c113967de6d5f3
Steve <pietschmanns@flggem.onmicrosoft.com>
2026-05-08 08:42:06 +0200

    started player

--- Game/src/main/java/model/Player.java
+++ Game/src/main/java/model/Player.java
@@ -1,12 +1,37 @@
 package model;
+import config.Config;
+import com.almasb.fxgl.dsl.FXGL;
+import javafx.geometry.Point2D;
 
 public class Player {
-    boolean sprinting;
+   //objects:
+    private Snake snake;
+    private int score = 0;
+    private String name;
 
+    private boolean sprinting = false;
     public void toggleSprinting(){ //TODO Noch_nicht_bekannt_ob_richtig
         this.sprinting = !this.sprinting;
     }
+    //TODO Methode
+
     public void setVecsFromMousePos{
-        //TODO Methode
+        ;
     }
+}
+//score:
+public void addScore(int amount){
+    score = score + amount;
+}
+public void substractScore(int amount){
+    score = (Math.max(0,score - amount));
+}
+
+//*/ Return_STH /*//
+public boolean isSprinting(){
+    return sprinting;
+}
+
+public int getScore(){
+    return score;
 }
