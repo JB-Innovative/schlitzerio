d8efceda73c36cfee08cf1d1b25bb11d9bc8f166
Matze1311 <kreser.m@gmx.de>
2026-06-12 08:25:15 +0200

    Irgendwelchen Text

---
+++ Game/src/main/java/GameOver.java
@@ -0,0 +1,42 @@
+import com.almasb.fxgl.app.scene.FXGLMenu;
+import com.almasb.fxgl.app.scene.MenuType;
+import com.almasb.fxgl.dsl.FXGL;
+import javafx.geometry.Pos;
+import javafx.scene.control.Button;
+import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
+import javafx.scene.text.Text;
+import config.Config;
+public class GameOver extends FXGLMenu {
+
+    public GameOver()
+    {
+        super(MenuType.MAIN_MENU);
+        Rectangle bg=new Rectangle(Config.WIDTH, Config.Color, Config.HEIGHT);
+        getContentRoot().getChildren().add(bg);
+
+        Vbox vbox= new VBox(20);
+        vbox.setAlignement(Pos.CENTER);
+        vbox.setPrefSize(Config.WIDTH, Config.HEIGHT);
+
+        Text title = new Text(Config.TITLE);
+        title.setfill(Color.LIMEGREEN);
+        title.setStyle("-fx-font-size: 64px; -fx-font-weight: bold;");
+
+        Button playAgain = new Button("NEU STARTEN");
+        playAgain.setStyle("-fx-font-size: 24px; -fx-base:#32CD32; -fx-text-fill: white;");
+        playAgain.setOnAction(e->fireNewGame());
+
+        Button menu = new Button("ZUM HAUPTMENU");
+        menu.setStyle("-fx-font-size: 24px; -fx-base: #CD5C5C; -fx-text-fill: white;");
+        menu.setOnAction( e-> fireExitToMainMenu());
+
+        Button exit = new Button("Spiel beenden");
+        exit.setStyle("-fx-font-size: 24px; -fx-base: #CD5C5C; -fx-text-fill: white;");
+        exit.setOnAction( e-> fireExit());
+
+        vbox.getChildren().addAll(title); playAgain, exit, menu);
+        getContentRoot().getChildren().add(vbox);
+    }
+}
